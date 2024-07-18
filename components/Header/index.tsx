6b61f67622d64b4c2bcbdaddc1413d33c3c5e5b1
Nick Verstocken <nick.verstocken@novemberfive.co>
2024-07-18 14:50:34 +0200

    chore: remove notification

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,14 +1,12 @@
-'use client';
+"use client";
 
-import LanguageSelector from '@/components/Header/LanguageSelector';
-import NotificationStrip from '@/components/Header/NotificationStrip';
-import type { CommonLayoutQuery } from '@/graphql/types/graphql';
-import type { GlobalPageProps } from '@/utils/globalPageProps';
-import { buildUrl } from '@/utils/globalPageProps';
-import { isEmptyDocument } from 'datocms-structured-text-utils';
-import Image from 'next/image';
-import Link from 'next/link';
-import { useEffect, useState } from 'react';
+import LanguageSelector from "@/components/Header/LanguageSelector";
+import type { CommonLayoutQuery } from "@/graphql/types/graphql";
+import type { GlobalPageProps } from "@/utils/globalPageProps";
+import { buildUrl } from "@/utils/globalPageProps";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 type Menu = {
   id: string;
@@ -27,11 +25,11 @@ const Header = ({ globalPageProps, data }: Props) => {
   const menuData: Menu[] = [];
 
   data.layout?.menu.map((item) => {
-    if (item.__typename === 'MenuDropdownRecord') {
+    if (item.__typename === "MenuDropdownRecord") {
       const dropdownItem = item;
       menuData.push({
-        id: '1',
-        title: dropdownItem.title || 'Other Items',
+        id: "1",
+        title: dropdownItem.title || "Other Items",
         newTab: false,
         submenu: dropdownItem.items.map((item) => {
           return {
@@ -55,9 +53,6 @@ const Header = ({ globalPageProps, data }: Props) => {
 
   // Navbar toggle
   const [navbarOpen, setNavbarOpen] = useState(false);
-  const [notificationStrip, setNotificationStrip] = useState(
-    !isEmptyDocument(data.layout?.notification),
-  );
 
   const navbarToggleHandler = () => {
     setNavbarOpen(!navbarOpen);
@@ -73,7 +68,7 @@ const Header = ({ globalPageProps, data }: Props) => {
     }
   };
   useEffect(() => {
-    window.addEventListener('scroll', handleStickyNavbar);
+    window.addEventListener("scroll", handleStickyNavbar);
   });
 
   // submenu handler
@@ -88,18 +83,11 @@ const Header = ({ globalPageProps, data }: Props) => {
 
   return (
     <>
-      {notificationStrip && (
-        <NotificationStrip
-          notification={data.layout?.notification}
-          globalPageProps={globalPageProps}
-          setNotificationStrip={setNotificationStrip}
-        />
-      )}
       <header
         className={`header left-0 z-40 flex w-full items-center bg-transparent ${
           sticky
-            ? 'fixed top-0 z-50 bg-white bg-opacity-80 shadow-sticky backdrop-blur-sm transition'
-            : `absolute ${notificationStrip ? 'top-10' : 'top-0'}`
+            ? "fixed top-0 z-50 bg-white bg-opacity-80 shadow-sticky backdrop-blur-sm transition"
+            : `absolute top-0`
         }`}
       >
         <div className="container">
@@ -108,7 +96,7 @@ const Header = ({ globalPageProps, data }: Props) => {
               <Link
                 href={buildUrl(globalPageProps)}
                 className={`header-logo block w-full ${
-                  sticky ? 'py-5 lg:py-2' : 'py-8'
+                  sticky ? "py-5 lg:py-2" : "py-8"
                 } `}
               >
                 {data.layout?.logo.url && (
@@ -133,17 +121,17 @@ const Header = ({ globalPageProps, data }: Props) => {
                 >
                   <span
                     className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 dark:bg-white ${
-                      navbarOpen ? ' top-[7px] rotate-45' : ' '
+                      navbarOpen ? " top-[7px] rotate-45" : " "
                     }`}
                   />
                   <span
                     className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 dark:bg-white ${
-                      navbarOpen ? 'opacity-0 ' : ' '
+                      navbarOpen ? "opacity-0 " : " "
                     }`}
                   />
                   <span
                     className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 dark:bg-white ${
-                      navbarOpen ? ' top-[-8px] -rotate-45' : ' '
+                      navbarOpen ? " top-[-8px] -rotate-45" : " "
                     }`}
                   />
                 </button>
@@ -151,8 +139,8 @@ const Header = ({ globalPageProps, data }: Props) => {
                   id="navbarCollapse"
                   className={`navbar absolute right-0 z-30 w-[250px] rounded border-[.5px] border-body-color/50 bg-white px-6 py-4 duration-300 dark:border-body-color/20 dark:bg-dark lg:visible lg:static lg:w-auto lg:border-none lg:!bg-transparent lg:p-0 lg:opacity-100 ${
                     navbarOpen
-                      ? 'visibility top-full opacity-100'
-                      : 'invisible top-[120%] opacity-0'
+                      ? "visibility top-full opacity-100"
+                      : "invisible top-[120%] opacity-0"
                   }`}
                 >
                   <ul className="block items-center lg:flex lg:space-x-12">
@@ -162,7 +150,7 @@ const Header = ({ globalPageProps, data }: Props) => {
                           <Link
                             href={buildUrl(globalPageProps, menuItem.path)}
                             className={
-                              'flex py-2 text-base text-dark group-hover:opacity-70 dark:text-white lg:mr-0 lg:inline-flex lg:px-0 lg:py-6'
+                              "flex py-2 text-base text-dark group-hover:opacity-70 dark:text-white lg:mr-0 lg:inline-flex lg:px-0 lg:py-6"
                             }
                           >
                             {menuItem.title}
@@ -185,14 +173,14 @@ const Header = ({ globalPageProps, data }: Props) => {
                             </a>
                             <div
                               className={`submenu relative left-0 top-full rounded-md bg-white transition-[top] duration-300 group-hover:opacity-100 dark:bg-dark lg:invisible lg:absolute lg:top-[110%] lg:block lg:w-[250px] lg:p-4 lg:opacity-0 lg:shadow-lg lg:group-hover:visible lg:group-hover:top-full ${
-                                openIndex === index ? 'block' : 'hidden'
+                                openIndex === index ? "block" : "hidden"
                               }`}
                             >
                               {menuItem.submenu?.map((submenuItem) => (
                                 <Link
                                   href={buildUrl(
                                     globalPageProps,
-                                    submenuItem.path,
+                                    submenuItem.path
                                   )}
                                   key={submenuItem.id}
                                   className="block rounded py-2.5 text-sm text-dark hover:opacity-70 dark:text-white lg:px-3"
